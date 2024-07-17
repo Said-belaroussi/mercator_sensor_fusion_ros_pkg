4532d7a17452a7b3571e8bdb5a666097393a50af
Said-belaroussi <said.belaroussi@ulb.be>
2024-07-17 18:10:20 +0200

    passing numpy array as ros parameter correctly

--- src/mercator_sensor_fusion_ros_pkg/pose_array_republisher.py
+++ src/mercator_sensor_fusion_ros_pkg/pose_array_republisher.py
@@ -10,13 +10,11 @@ class PoseArrayRepublisherNode:
         rospy.init_node('pose_array_republisher_node', anonymous=True)
 
         # Read the transformation matrix from ROS parameters
-        self.transform_matrix = rospy.get_param('~transform_matrix', [
-            [0.99675996, 0.08043372, -0.05389885],
-            [-0.08043372, 0.99675996, 0.16321373],
-            [0.0, 0.0, 1.0]
-        ])
-        self.transform_matrix = np.array(self.transform_matrix)
+        self.transform_matrix = rospy.get_param('~transform_matrix', 
+        "[[0.99675996, 0.08043372, -0.05389885], [-0.08043372, 0.99675996, 0.16321373], [0.0, 0.0, 1.0]]")
 
+        # Convert the string to a numpy array
+        self.transform_matrix = self.string_to_2d_array(self.transform_matrix)
         # Define the publisher
         self.pub = rospy.Publisher('cam_poses_base_link', PoseArray, queue_size=10)
 
@@ -24,7 +22,26 @@ class PoseArrayRepublisherNode:
         self.sub = rospy.Subscriber('cam_poses', PoseArray, self.callback)
 
         self.run()
-        
+
+    def string_to_2d_array(string):
+        """
+        Converts a string representation of a 2D array to a NumPy array.
+        Args:
+            string: The string representation of the 2D array.
+        Returns:
+            A 2D NumPy array representing the input string.
+        Raises:
+            ValueError: If the string format is invalid.
+        """
+        try:
+            # Use eval to convert the string to a nested list
+            array_list = eval(string)
+        except (NameError, SyntaxError):
+            raise ValueError("Invalid string format. Please use proper list of lists syntax.")
+
+        # Convert the nested list to a NumPy array
+        return np.array(array_list)
+
     def callback(self, pose_array_msg):
         # Change the frame_id to "base_link_40"
         pose_array_msg.header.frame_id = "base_link_40"
