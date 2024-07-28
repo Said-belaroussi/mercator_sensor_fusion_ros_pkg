f4021541d6c5bff606a7c01c6a1419153fb0431f
Said-belaroussi <said.belaroussi@ulb.be>
2024-07-28 10:51:31 +0200

    finalizing fusion launch file

--- src/mercator_sensor_fusion_ros_pkg/odometry_corrector.py
+++ src/mercator_sensor_fusion_ros_pkg/odometry_corrector.py
@@ -5,15 +5,18 @@ from sensor_msgs.msg import Imu
 from nav_msgs.msg import Odometry
 from tf.transformations import euler_from_quaternion
 from math import sin, cos, atan2
+import tf
 
 class OdometryCorrectorNode:
     def __init__(self):
         rospy.init_node('odometry_corrector_node', anonymous=True)
+        self.frame_id = rospy.get_param('~frame_id', 'odom')
+        self.child_frame_id = rospy.get_param('~child_frame_id', 'base_link')
         self.imu_subscriber = rospy.Subscriber('rvr/imu', Imu, self.imu_callback)
         self.imu_publisher = rospy.Publisher('imu', Imu, queue_size=10)
         self.odom_subscriber = rospy.Subscriber('/rvr/odom', Odometry, self.odom_callback)
         self.odom_publisher = rospy.Publisher('/rvr/odom/correct', Odometry, queue_size=10)
-        # self.odom_imu_mixed_publisher = rospy.Publisher('/rvr/odom/odom_imu_mixed', Odometry, queue_size=10)
+        self.tf_broadcaster = tf.TransformBroadcaster()
         self.initial_orientation = None
         self.initial_yaw = None
         self.initial_x = None
@@ -25,7 +28,7 @@ class OdometryCorrectorNode:
 
     def imu_callback(self, imu_msg):
         # Modify the frame_id field to "base_link"
-        imu_msg.header.frame_id = "base_link"
+        imu_msg.header.frame_id = self.child_frame_id
         # Publish the modified IMU message
         self.imu_publisher.publish(imu_msg)
 
@@ -62,8 +65,8 @@ class OdometryCorrectorNode:
                 new_x = x * cos(self.initial_yaw) - y * sin(self.initial_yaw)
                 new_y = x * sin(self.initial_yaw) + y * cos(self.initial_yaw)
                 # Modify the frame_id and child_frame_id fields
-                odom_msg.header.frame_id = "odom"
-                odom_msg.child_frame_id = "base_link"
+                odom_msg.header.frame_id = self.frame_id
+                odom_msg.child_frame_id = self.child_frame_id
                 # Update the x, y positions
                 odom_msg.pose.pose.position.x = new_x
                 odom_msg.pose.pose.position.y = new_y
@@ -72,10 +75,19 @@ class OdometryCorrectorNode:
                 odom_msg.pose.pose.orientation = self.last_orientation
                 self.odom_publisher.publish(odom_msg)
 
-                # # Publish the odometry message with IMU orientation mixed in
-                # odom_msg.pose.pose.orientation = self.last_orientation
-                # self.odom_imu_mixed_publisher.publish(odom_msg)
-
+                # Broadcast the transform
+                self.tf_broadcaster.sendTransform(
+                    (new_x, new_y, 0),
+                    (
+                        self.last_orientation.x,
+                        self.last_orientation.y,
+                        self.last_orientation.z,
+                        self.last_orientation.w,
+                    ),
+                    rospy.Time.now(),
+                    self.child_frame_id,
+                    self.frame_id
+                )
 
     def run(self):
         rospy.spin()
