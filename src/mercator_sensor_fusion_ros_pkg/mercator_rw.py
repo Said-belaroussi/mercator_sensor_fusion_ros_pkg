4f74111bc648109907e33d4eb8c9e7061626edf8
Said-belaroussi <said.belaroussi@ulb.be>
2024-08-02 17:42:17 +0200

    update random walk

--- src/mercator_sensor_fusion_ros_pkg/mercator_rw.py
+++ src/mercator_sensor_fusion_ros_pkg/mercator_rw.py
@@ -24,6 +24,10 @@ class MercatorRwNode:
 
         self.message_counter = 0  # Counter to track messages
 
+        self.straight_counter = 0  # Counter to track consecutive straight movements
+        self.last_stop_time = 0  # Time when the last stop occurred
+        self.can_stop_again = True  # Flag to control if the robot can stop again
+
         self.run()
 
     def string_to_list(self, string):
@@ -91,10 +95,21 @@ class MercatorRwNode:
     
     def obstacle_avoidance(self, ranges, idx, dist):
         data_to_send = Float32MultiArray()
+        data_to_send.layout.dim.append(MultiArrayDimension())
+        data_to_send.layout.dim[0].label = 'wheel_vel'
+        data_to_send.layout.dim[0].size = 2
+        data_to_send.layout.dim[0].stride = 1
+        data_to_send.layout.data_offset = 0
 
         if dist > self.min_dist_threshold:
             left, right = self.go_straight()
+            data_to_send.data = [left, right]
+            self.pub.publish(data_to_send)
+
+            self.straight_counter += 1
         else:
+            self.straight_counter = 0  # Reset straight counter if not going straight
+
             # Check all ranges for specific angle conditions
             left_turn = False
             right_turn = False
@@ -103,9 +118,9 @@ class MercatorRwNode:
                 angle = self.sensor_angles[i]
                 if range_msg.range < self.min_dist_threshold:
                     if 45 <= angle <= 90:
-                        left_turn = True
-                    elif -90 <= angle <= -45:
                         right_turn = True
+                    elif -90 <= angle <= -45:
+                        left_turn = True
 
             if left_turn:
                 left, right = self.go_left()
@@ -124,11 +139,6 @@ class MercatorRwNode:
             
             # Send the chosen direction
             data_to_send.data = [left, right]
-            data_to_send.layout.dim.append(MultiArrayDimension())
-            data_to_send.layout.dim[0].label = 'wheel_vel'
-            data_to_send.layout.dim[0].size = 2
-            data_to_send.layout.dim[0].stride = 1
-            data_to_send.layout.data_offset = 0
             self.pub.publish(data_to_send)
 
             # Sleep for the duration
@@ -137,13 +147,25 @@ class MercatorRwNode:
             # Stop after the duration
             left, right = 0, 0
 
-        data_to_send.data = [left, right]
-        data_to_send.layout.dim.append(MultiArrayDimension())
-        data_to_send.layout.dim[0].label = 'wheel_vel'
-        data_to_send.layout.dim[0].size = 2
-        data_to_send.layout.dim[0].stride = 1
-        data_to_send.layout.data_offset = 0
-        self.pub.publish(data_to_send)
+        # Check if the robot needs to stop
+        current_time = time.time()
+        if self.straight_counter >= 3 and self.can_stop_again:
+            rospy.loginfo("Stopping robot for a random amount of time")
+            left, right = 0, 0
+
+            data_to_send.data = [left, right]
+            self.pub.publish(data_to_send)
+
+            stop_duration = random.uniform(5, 10)
+            time.sleep(stop_duration)
+
+            self.straight_counter = 0
+            self.last_stop_time = current_time
+            self.can_stop_again = False
+
+        # Check if the stop cooldown period has passed
+        if not self.can_stop_again and (current_time - self.last_stop_time) > 5:
+            self.can_stop_again = True
 
     def run(self):
         rospy.spin()
