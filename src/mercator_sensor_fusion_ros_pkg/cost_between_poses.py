6c2a30e878aa6e81a950e60172248a8bfffb0fbd
Said-belaroussi <said.belaroussi@ulb.be>
2024-08-09 21:39:01 +0200

    updated cost_between_poses

--- src/mercator_sensor_fusion_ros_pkg/cost_between_poses.py
+++ src/mercator_sensor_fusion_ros_pkg/cost_between_poses.py
@@ -1,48 +1,117 @@
 #!/usr/bin/env python
 
 import rospy
+import rosbag
 from geometry_msgs.msg import PoseArray
 from scipy.optimize import linear_sum_assignment
 import numpy as np
-import time
+import threading
 
 class CostBetweenPosesNode:
-    def __init__(self):
+    def __init__(self, experiment_bag_path='experiment_bag_path', ground_truth_bag_path='ground_truth_bag_path'):
         rospy.init_node('cost_between_poses_node', anonymous=True)
+
+        # Get rosbag paths from ROS parameters
+        self.experiment_bag_path = rospy.get_param('~experiment_bag_path', experiment_bag_path)
+        self.ground_truth_bag_path = rospy.get_param('~ground_truth_bag_path', ground_truth_bag_path)
+
+        # Buffers for poses and ground truth
         self.experiment_buffer = []
-        self.ground_truth_buffer = []
-        self.latest_experiment_poses = None
-        self.buffer_duration = rospy.get_param('~buffer_duration', 60)  # Buffer duration in seconds
+        self.cam_buffer = []
+        self.lidar_buffer = []
+        self.ground_truth_buffer_for_experiment = []
+        self.ground_truth_buffer_for_cam = []
+        self.ground_truth_buffer_for_lidar = []
+
         self.max_shift_messages = rospy.get_param('~max_shift_messages', 20)  # Max shift in number of messages
         self.shift_step = rospy.get_param('~shift_step', 1)
-        self.timer = rospy.Timer(rospy.Duration(self.buffer_duration), self.compute_costs)
+        self.time_tolerance = rospy.get_param('~time_tolerance', 0.05)  # Tolerance in seconds for syncing messages
 
-        self.experiment_sub = rospy.Subscriber('experiment_poses', PoseArray, self.experiment_callback, queue_size=1)
-        self.ground_truth_sub = rospy.Subscriber('ground_truth_poses', PoseArray, self.ground_truth_callback, queue_size=1)
+        # Thread to read and process rosbag data
+        self.read_bag_thread = threading.Thread(target=self.read_bags)
+        self.read_bag_thread.start()
 
         self.run()
 
-    def experiment_callback(self, msg):
-        if self.latest_experiment_poses:
-            current_time = time.time()
-            self.ground_truth_buffer.append((current_time, self.latest_experiment_poses))
-            self.experiment_buffer.append((current_time, msg.poses))
+    def read_bags(self):
+        # Open rosbag files
+        experiment_bag = rosbag.Bag(self.experiment_bag_path, 'r')
+        ground_truth_bag = rosbag.Bag(self.ground_truth_bag_path, 'r')
 
-    def ground_truth_callback(self, msg):
-        self.latest_experiment_poses = msg.poses
+        # Read all messages from the bags
+        experiment_messages = list(experiment_bag.read_messages(topics=['fused_poses', 'cam_poses', 'lidar_poses']))
+        ground_truth_messages = list(ground_truth_bag.read_messages(topics=['ground_truth_poses']))
 
-    def compute_costs(self, event):
-        current_time = time.time()
-        self.experiment_buffer = [msg for msg in self.experiment_buffer if current_time - msg[0] <= self.buffer_duration]
-        self.ground_truth_buffer = [msg for msg in self.ground_truth_buffer if current_time - msg[0] <= self.buffer_duration]
+        # Get the first timestamps
+        first_experiment_time = experiment_messages[0][2].to_sec()
+        first_ground_truth_time = ground_truth_messages[0][2].to_sec()
 
-        if not self.experiment_buffer or not self.ground_truth_buffer:
-            rospy.loginfo("Buffers are empty or have insufficient data")
-            return
+        # Compute the time offset between the two bags
+        time_offset = first_experiment_time - first_ground_truth_time
 
+        # Process each message in order of timestamp
+        for topic, msg, t in experiment_messages:
+            adjusted_time = t.to_sec() - time_offset
+            if topic == 'fused_poses':
+                self.experiment_callback(msg, adjusted_time)
+            elif topic == 'cam_poses':
+                self.cam_callback(msg, adjusted_time)
+            elif topic == 'lidar_poses':
+                self.lidar_callback(msg, adjusted_time)
+
+        for topic, msg, t in ground_truth_messages:
+            self.ground_truth_callback(msg, t.to_sec())
+
+        experiment_bag.close()
+        ground_truth_bag.close()
+
+        # After processing all messages, compute costs
+        self.compute_costs()
+
+    def experiment_callback(self, msg, timestamp):
+        gt_pose = self.find_closest_ground_truth(timestamp, self.ground_truth_buffer_for_experiment)
+        if gt_pose:
+            self.experiment_buffer.append((timestamp, msg.poses))
+            self.ground_truth_buffer_for_experiment.append((timestamp, gt_pose))
+
+    def cam_callback(self, msg, timestamp):
+        gt_pose = self.find_closest_ground_truth(timestamp, self.ground_truth_buffer_for_cam)
+        if gt_pose:
+            self.cam_buffer.append((timestamp, msg.poses))
+            self.ground_truth_buffer_for_cam.append((timestamp, gt_pose))
+
+    def lidar_callback(self, msg, timestamp):
+        gt_pose = self.find_closest_ground_truth(timestamp, self.ground_truth_buffer_for_lidar)
+        if gt_pose:
+            self.lidar_buffer.append((timestamp, msg.poses))
+            self.ground_truth_buffer_for_lidar.append((timestamp, gt_pose))
+
+    def ground_truth_callback(self, msg, timestamp):
+        # Append ground truth poses without filtering
+        self.ground_truth_buffer_for_experiment.append((timestamp, msg.poses))
+        self.ground_truth_buffer_for_cam.append((timestamp, msg.poses))
+        self.ground_truth_buffer_for_lidar.append((timestamp, msg.poses))
+
+    def find_closest_ground_truth(self, timestamp, ground_truth_buffer):
+        closest_pose = None
+        min_time_diff = float('inf')
+
+        for gt_time, gt_pose in ground_truth_buffer:
+            time_diff = abs(gt_time - timestamp)
+            if time_diff < self.time_tolerance and time_diff < min_time_diff:
+                min_time_diff = time_diff
+                closest_pose = gt_pose
+
+        return closest_pose
+
+    def compute_costs(self):
+        # Compute costs for each pair of buffers
+        self.compute_cost_for_pair(self.experiment_buffer, self.ground_truth_buffer_for_experiment, "fused_poses")
+        self.compute_cost_for_pair(self.cam_buffer, self.ground_truth_buffer_for_cam, "cam_poses")
+        self.compute_cost_for_pair(self.lidar_buffer, self.ground_truth_buffer_for_lidar, "lidar_poses")
+
+    def compute_cost_for_pair(self, buffer_a, buffer_b, label):
         min_average_cost = float('inf')
-        min_average_x_cost = float('inf')
-        min_average_y_cost = float('inf')
         original_min_average_cost = float('inf')
         original_flag = False
         optimal_shift = 0
@@ -50,75 +119,63 @@ class CostBetweenPosesNode:
 
         for shift in range(-self.max_shift_messages, self.max_shift_messages + 1, self.shift_step):
             if shift < 0:
-                shifted_experiment_buffer = self.experiment_buffer[-shift:]
-                shifted_ground_truth_buffer = self.ground_truth_buffer[:shift]
+                shifted_buffer_a = buffer_a[-shift:]
+                shifted_buffer_b = buffer_b[:shift]
             elif shift > 0:
-                shifted_experiment_buffer = self.experiment_buffer[:-shift]
-                shifted_ground_truth_buffer = self.ground_truth_buffer[shift:]
+                shifted_buffer_a = buffer_a[:-shift]
+                shifted_buffer_b = buffer_b[shift:]
             else:
-                shifted_experiment_buffer = self.experiment_buffer
-                shifted_ground_truth_buffer = self.ground_truth_buffer
+                shifted_buffer_a = buffer_a
+                shifted_buffer_b = buffer_b
                 original_flag = True
 
             total_cost = 0
-            total_x_cost = 0
-            total_y_cost = 0
             count = 0
 
-            for exp_msg, gt_msg in zip(shifted_experiment_buffer, shifted_ground_truth_buffer):
-                cost, x_cost, y_cost = self.calculate_cost(exp_msg[1], gt_msg[1])
-                if cost != None:
+            for a_msg, b_msg in zip(shifted_buffer_a, shifted_buffer_b):
+                cost = self.calculate_cost(a_msg[1], b_msg[1])
+                if cost is not None:
                     total_cost += cost
-                    total_x_cost += x_cost
-                    total_y_cost += y_cost
                     count += 1
 
             if count > 0:
                 average_cost = total_cost / count
-                average_x_cost = total_x_cost / count
-                average_y_cost = total_y_cost / count
                 if average_cost < min_average_cost:
                     min_average_cost = average_cost
                     optimal_shift = shift
-                    perfect_delay = shifted_experiment_buffer[0][0] - shifted_ground_truth_buffer[0][0]
-                    min_average_x_cost = average_x_cost
-                    min_average_y_cost = average_y_cost
-                    if original_flag:
-                        original_min_average_cost = min_average_cost
-                        original_flag = False
+                    perfect_delay = shifted_buffer_a[0][0] - shifted_buffer_b[0][0]
 
-        rospy.loginfo("Minimum average cost: %f at shift: %d and delay %f", min_average_cost, optimal_shift, perfect_delay)
-        rospy.loginfo("Original minimum average cost: %f", original_min_average_cost)
-        rospy.loginfo("Average x cost: %f, Average y cost: %f", min_average_x_cost, min_average_y_cost)
+                if original_flag:
+                    original_min_average_cost = average_cost
+                    original_flag = False
 
-    def calculate_cost(self, experiment_poses, ground_truth_poses):
-        experiment_poses = np.array([[pose.position.x, pose.position.y] for pose in experiment_poses])
-        ground_truth_poses = np.array([[pose.position.x, pose.position.y] for pose in ground_truth_poses])
+        rospy.loginfo("[%s] Minimum average cost: %f at shift: %d and delay %f", label, min_average_cost, optimal_shift, perfect_delay)
+        rospy.loginfo("[%s] Original minimum average cost: %f", label, original_min_average_cost)
 
-        if experiment_poses.shape[0] == 0 or ground_truth_poses.shape[0] == 0:
-            return None, None, None
+    def calculate_cost(self, poses_a, poses_b):
+        poses_a = np.array([[pose.position.x, pose.position.y] for pose in poses_a])
+        poses_b = np.array([[pose.position.x, pose.position.y] for pose in poses_b])
 
-        cost_matrix = np.linalg.norm(experiment_poses[:, np.newaxis] - ground_truth_poses, axis=2)
+        if poses_a.shape[0] == 0 or poses_b.shape[0] == 0:
+            return None
+
+        cost_matrix = np.linalg.norm(poses_a[:, np.newaxis] - poses_b, axis=2)
         row_ind, col_ind = linear_sum_assignment(cost_matrix)
 
         # Compute the average cost
         total_cost = cost_matrix[row_ind, col_ind].sum() / len(row_ind)
 
-        # TODO: Compute if the cost is more along the x or y axis
-        x_cost = 0
-        y_cost = 0
-        for i in range(len(row_ind)):
-            x_cost += abs(experiment_poses[row_ind[i]][0] - ground_truth_poses[col_ind[i]][0])
-            y_cost += abs(experiment_poses[row_ind[i]][1] - ground_truth_poses[col_ind[i]][1])
-
-        x_cost /= len(row_ind)
-        y_cost /= len(row_ind)
-
-        return total_cost, x_cost, y_cost
+        return total_cost
 
     def run(self):
         rospy.spin()
 
 if __name__ == '__main__':
-    node = CostBetweenPosesNode()
+    # Parse command line arguments
+    import argparse
+    parser = argparse.ArgumentParser(description='Compute cost between poses')
+    parser.add_argument('experiment_bag_path', type=str, help='Path to the rosbag file with experiment poses')
+    parser.add_argument('ground_truth_bag_path', type=str, help='Path to the rosbag file with ground truth poses')
+    args = parser.parse_args()
+    node = CostBetweenPosesNode(args.experiment_bag_path, args.ground_truth_bag_path)
     node.run()
--- src/mercator_sensor_fusion_ros_pkg/cost_between_poses_old.py
+++ src/mercator_sensor_fusion_ros_pkg/cost_between_poses_old.py
@@ -4,49 +4,105 @@ import rospy
 from geometry_msgs.msg import PoseArray
 from scipy.optimize import linear_sum_assignment
 import numpy as np
+import time
 
 class CostBetweenPosesNode:
     def __init__(self):
         rospy.init_node('cost_between_poses_node', anonymous=True)
-        self.experiment_poses = None
-        self.ground_truth_poses = None
-        self.average_cost = 0.0
-        self.iterations = 0
-        self.experiment_sub = rospy.Subscriber('experiment_poses', PoseArray, self.experiment_callback)
-        self.ground_truth_sub = rospy.Subscriber('ground_truth_poses', PoseArray, self.ground_truth_callback)
+        self.experiment_buffer = []
+        self.ground_truth_buffer = []
+        self.latest_experiment_poses = None
+        self.buffer_duration = rospy.get_param('~buffer_duration', 60)  # Buffer duration in seconds
+        self.max_shift_messages = rospy.get_param('~max_shift_messages', 20)  # Max shift in number of messages
+        self.shift_step = rospy.get_param('~shift_step', 1)
+        self.timer = rospy.Timer(rospy.Duration(self.buffer_duration), self.compute_costs)
+
+        self.experiment_sub = rospy.Subscriber('experiment_poses', PoseArray, self.experiment_callback, queue_size=1)
+        self.ground_truth_sub = rospy.Subscriber('ground_truth_poses', PoseArray, self.ground_truth_callback, queue_size=1)
 
         self.run()
 
     def experiment_callback(self, msg):
-        self.experiment_poses = msg.poses
-        if self.ground_truth_poses:
-            self.match_poses()
+        if self.latest_experiment_poses:
+            current_time = time.time()
+            self.ground_truth_buffer.append((current_time, self.latest_experiment_poses))
+            self.experiment_buffer.append((current_time, msg.poses))
 
     def ground_truth_callback(self, msg):
-        self.ground_truth_poses = msg.poses
+        self.latest_experiment_poses = msg.poses
 
-    def match_poses(self):
-        experiment_poses = np.array([[pose.position.x, pose.position.y] for pose in self.experiment_poses])
-        ground_truth_poses = np.array([[pose.position.x, pose.position.y] for pose in self.ground_truth_poses])
+    def compute_costs(self, event):
+        current_time = time.time()
+        self.experiment_buffer = [msg for msg in self.experiment_buffer if current_time - msg[0] <= self.buffer_duration]
+        self.ground_truth_buffer = [msg for msg in self.ground_truth_buffer if current_time - msg[0] <= self.buffer_duration]
 
-        # Check that both poses arrays are available
-        if experiment_poses.shape[0] == 0 or ground_truth_poses.shape[0] == 0:
-            rospy.loginfo("No poses available")
+        if not self.experiment_buffer or not self.ground_truth_buffer:
+            rospy.loginfo("Buffers are empty or have insufficient data")
             return
-        rospy.loginfo(experiment_poses)
-        rospy.loginfo(ground_truth_poses)
-        cost_matrix = np.linalg.norm(experiment_poses[:, np.newaxis] - ground_truth_poses, axis=2)
-        rospy.loginfo(cost_matrix)
-        row_ind, col_ind = linear_sum_assignment(cost_matrix)
-        total_cost = cost_matrix[row_ind, col_ind].sum()
-        self.average_cost = (self.average_cost * self.iterations + total_cost) / (self.iterations + 1)
-        self.iterations += 1
 
-        rospy.loginfo("Total cost of optimal matching: %f", total_cost)
-        rospy.loginfo("Average cost since the beginning: %f", self.average_cost)
+        min_average_cost = float('inf')
+        min_average_x_cost = float('inf')
+        min_average_y_cost = float('inf')
+        original_min_average_cost = float('inf')
+        original_flag = False
+        optimal_shift = 0
+        perfect_delay = 0
+
+        for shift in range(-self.max_shift_messages, self.max_shift_messages + 1, self.shift_step):
+            if shift < 0:
+                shifted_experiment_buffer = self.experiment_buffer[-shift:]
+                shifted_ground_truth_buffer = self.ground_truth_buffer[:shift]
+            elif shift > 0:
+                shifted_experiment_buffer = self.experiment_buffer[:-shift]
+                shifted_ground_truth_buffer = self.ground_truth_buffer[shift:]
+            else:
+                shifted_experiment_buffer = self.experiment_buffer
+                shifted_ground_truth_buffer = self.ground_truth_buffer
+                original_flag = True
+
+            total_cost = 0
+            count = 0
+
+            for exp_msg, gt_msg in zip(shifted_experiment_buffer, shifted_ground_truth_buffer):
+                cost = self.calculate_cost(exp_msg[1], gt_msg[1])
+                if cost != None:
+                    total_cost += cost
+                    count += 1
+
+            if count > 0:
+                average_cost = total_cost / count
+                average_x_cost = total_x_cost / count
+                average_y_cost = total_y_cost / count
+                if average_cost < min_average_cost:
+                    min_average_cost = average_cost
+                    optimal_shift = shift
+                    perfect_delay = shifted_experiment_buffer[0][0] - shifted_ground_truth_buffer[0][0]
+
+                if original_flag:
+                    original_min_average_cost = average_cost
+                    original_flag = False
+
+        rospy.loginfo("Minimum average cost: %f at shift: %d and delay %f", min_average_cost, optimal_shift, perfect_delay)
+        rospy.loginfo("Original minimum average cost: %f", original_min_average_cost)
+
+    def calculate_cost(self, experiment_poses, ground_truth_poses):
+        experiment_poses = np.array([[pose.position.x, pose.position.y] for pose in experiment_poses])
+        ground_truth_poses = np.array([[pose.position.x, pose.position.y] for pose in ground_truth_poses])
+
+        if experiment_poses.shape[0] == 0 or ground_truth_poses.shape[0] == 0:
+            return None, None, None
+
+        cost_matrix = np.linalg.norm(experiment_poses[:, np.newaxis] - ground_truth_poses, axis=2)
+        row_ind, col_ind = linear_sum_assignment(cost_matrix)
+
+        # Compute the average cost
+        total_cost = cost_matrix[row_ind, col_ind].sum() / len(row_ind)
+
+        return total_cost
 
     def run(self):
         rospy.spin()
 
 if __name__ == '__main__':
-    main()
+    node = CostBetweenPosesNode()
+    node.run()
